087a9ad88c6852348765c1a06b26f6b631d1ffdc
Elliot Chance <elliotchance@gmail.com>
2025-11-04 16:49:30 -0500

    WIP

--- Sources/MixNerd/TracklistEditorWebView.swift
+++ Sources/MixNerd/TracklistEditorWebView.swift
@@ -17,11 +17,11 @@ struct TracklistEditorWebView: View {
         string:
             "https://www.1001tracklists.com/tracklist/2klx8j7t/armin-van-buuren-ruben-de-ronde-ferry-corsten-a-state-of-trance-1248-ade-special-amsterdam-dance-event-netherlands-2025-10-23.html"
     )!
-    private let tracklistWebViewWidth = 350.0
+    private let tracklistWebViewWidth = 200.0
     
     var body: some View {
         GeometryReader { geometry in
-            HStack(spacing: 0) {
+            VStack(alignment: .leading, spacing: 0) {
                 TracklistWebView(
                     url: initialURL,
                     setTracklist: { [state] tl in
@@ -30,27 +30,43 @@ struct TracklistEditorWebView: View {
                         }
                     }
                 )
-                .frame(width: geometry.size.width - tracklistWebViewWidth, height: geometry.size.height)
+                .frame(width: geometry.size.width, height: geometry.size.height - tracklistWebViewWidth)
                 .border(Color.gray.opacity(0.3))
                 .clipped()
 
                 Divider()
 
                 HStack {
-                    if let tracklist = state.tracklist {
-                        TracklistView(tracklist: Binding(get: { tracklist }, set: { state.setTracklist($0) }))
-                    } else {
-                        VStack(alignment: .leading, spacing: 8) {
-                            Text("Not a track list page")
-                                .font(.headline)
-                            Text("Open a URL starting with \"https://www.1001tracklists.com/tracklist/\" to see track details.")
-                                .foregroundColor(.secondary)
+                    VStack {
+                        if let tracklist = state.tracklist {
+                            TracklistView(tracklist: Binding(get: { tracklist }, set: { state.setTracklist($0) }))
+                        } else {
+                            VStack(alignment: .leading, spacing: 8) {
+                                Text("Not a track list page")
+                                    .font(.headline)
+                                Text("Open a URL starting with \"https://www.1001tracklists.com/tracklist/\" to see track details.")
+                                    .foregroundColor(.secondary)
+                            }
+                            .padding()
                         }
-                        .padding()
                     }
+                    .frame(width: geometry.size.width * 0.5, height: geometry.size.height)
+
+                    VStack {
+                        if let tracklist = state.tracklist {
+                            TracklistView(tracklist: Binding(get: { tracklist }, set: { state.setTracklist($0) }))
+                        } else {
+                            VStack(alignment: .leading, spacing: 8) {
+                                Text("Not a track list page")
+                                    .font(.headline)
+                                Text("Open a URL starting with \"https://www.1001tracklists.com/tracklist/\" to see track details.")
+                                    .foregroundColor(.secondary)
+                            }
+                            .padding()
+                        }
+                    }
+                    .frame(width: geometry.size.width * 0.5, height: geometry.size.height)
                 }
-                .frame(width: tracklistWebViewWidth, height: geometry.size.height)
-                .background(Color(NSColor.windowBackgroundColor))
             }
         }
     }
