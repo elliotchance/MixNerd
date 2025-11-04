cc540b08478dcfe9c6c22f39e62bd473ed3a72f6
Elliot Chance <elliotchance@gmail.com>
2025-11-04 10:41:10 -0500

    WIP

--- Sources/MixNerd/WebView.swift
+++ Sources/MixNerd/WebView.swift
@@ -4,11 +4,27 @@ import WebKit
 struct CustomWebView: NSViewRepresentable { // macOS, not iOS
     let url: URL
 
+    func makeCoordinator() -> Coordinator {
+        Coordinator()
+    }
+
     func makeNSView(context: Context) -> WKWebView {
-        WKWebView()
+        let webView = WKWebView()
+        webView.navigationDelegate = context.coordinator
+        return webView
     }
 
     func updateNSView(_ nsView: WKWebView, context: Context) {
         nsView.load(URLRequest(url: url))
+    }
+
+    class Coordinator: NSObject, WKNavigationDelegate {
+
+        func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+            let currentTitle = webView.title ?? ""
+            DispatchQueue.main.async {
+                UserDefaults.standard.set(currentTitle, forKey: "pageTitle")
+            }
+        }
     }
 }
