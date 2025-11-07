96ef26072f71c17bc1356e83fb863341b25f6a3e
Elliot Chance <elliotchance@gmail.com>
2025-11-07 00:47:16 -0500

    WIP

--- Sources/MixNerd/TracklistView.swift
+++ Sources/MixNerd/TracklistView.swift
@@ -1,14 +1,19 @@
 import SwiftUI
 
 struct TracklistView: View {
-  @AppStorage("bgArtURLString") private var bgArtURLString: String = ""
   @Binding var tracklist: Tracklist
   let artworkSize = 200.0 - 30.0 // in pixels
 
   var body: some View {
     HStack(alignment: .top, spacing: 0) {
       Group {
-        if let url = URL(string: bgArtURLString), !bgArtURLString.isEmpty {
+        if let artwork = tracklist.artwork {
+          Image(nsImage: artwork)
+            .resizable()
+            .scaledToFill()
+            .frame(width: artworkSize, height: artworkSize)
+            .clipped()
+        } else if let url = URL(string: tracklist.artworkURL), !tracklist.artworkURL.isEmpty {
           AsyncImage(url: url) { phase in
             switch phase {
             case .empty:
@@ -41,19 +46,23 @@ struct TracklistView: View {
 
       ScrollView {
         Form {
-          TextField("Date", text: Binding(get: { tracklist.date }, set: { tracklist.date = $0 }))
-            .disabled(!tracklist.editable)
+          // TextField("Date", text: Binding(get: { tracklist.date }, set: { tracklist.date = $0 }))
+          //   .disabled(!tracklist.editable)
           TextField("Artist", text: Binding(get: { tracklist.artist }, set: { tracklist.artist = $0 }))
             .disabled(!tracklist.editable)
           TextField("Title", text: Binding(get: { tracklist.title }, set: { tracklist.title = $0 }))
             .disabled(!tracklist.editable)
-          TextField("Source", text: Binding(get: { tracklist.source }, set: { tracklist.source = $0 }))
+          TextField("Grouping", text: Binding(get: { tracklist.source }, set: { tracklist.source = $0 }))
+            .disabled(!tracklist.editable)
+          TextField("Genre", text: Binding(get: { tracklist.genre }, set: { tracklist.genre = $0 }))
             .disabled(!tracklist.editable)
 
           ForEach(Array(tracklist.tracks.enumerated()), id: \.element.id) { index, track in
             HStack(alignment: .top) {
               VStack {
                 Text(String(format: "%02d", index + 1))
+                .controlSize(.large)
+                
                 TextField("", text: Binding(get: { track.time }, set: { _ in }))
                   .disabled(!tracklist.editable)
                   .frame(width: 80)
