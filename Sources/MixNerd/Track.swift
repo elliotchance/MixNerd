6018630fa8f18807ed7a0f02b0b9871a3f45c79d
Elliot Chance <elliotchance@gmail.com>
2025-11-11 15:09:55 -0500

    WIP

--- Sources/MixNerd/Track.swift
+++ Sources/MixNerd/Track.swift
@@ -2,15 +2,26 @@ import SwiftUI
 
 struct Track: Identifiable {
   var id = UUID()
+
+  // This is the start time of the track (not the duration).
   var time: String
+  var timeIsEstimated: Bool = false
+
   var artist: String
   var title: String
   var label: String = ""  // e.g. ARMIND
 
   func String() -> String {
     if label.isEmpty {
-      return "[\(time)] \(artist) - \(title)"
+      return "[\(formattedTime())] \(artist) - \(title)"
     }
-    return "[\(time)] \(artist) - \(title) [\(label)]"
+    return "[\(formattedTime())] \(artist) - \(title) [\(label)]"
+  }
+
+  func formattedTime() -> String {
+    if timeIsEstimated {
+      return "~\(time)"
+    }
+    return time
   }
 }
